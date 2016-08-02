78c3cf9dbbe158463e625a2f2fc74a626362cd4a
Kelly Kearney <kakearney@gmail.com>
2016-08-02 16:19:16 -0700

    Updated code for new ecopathmodel objects
    This code now expects an ecopathmodel object as input, rather than
    the older ecopath input structure.  Calculations have been streamlined
    based on new ecopathmodel methods.

--- ecopathsankey.m
+++ ecopathsankey.m
@@ -1,8 +1,8 @@
-function G = ecopathsankey(Ewein, file, varargin)
+function Json = ecopathsankey(EM, file, varargin)
 %ECOPATHSANKEY Create JSON file for use with d3 Ecopath sankey diagrams
 %
-% G = ecopathsankey(Ewein, file)
-% G = ecopathsankey(Ewein, file, p1, v1, ...)
+% Json = ecopathsankey(EM, file)
+% Json = ecopathsankey(EM, file, p1, v1, ...)
 %
 % Input variables:
 %
@@ -31,10 +31,10 @@ function G = ecopathsankey(Ewein, file, varargin)
 %
 % Output variables:
 %
-%   G:          structure of nodes and links properties, mirroring that
+%   Json:       structure of nodes and links properties, mirroring that
 %               written to the JSON file.
 
-% Copyright 2015 Kelly Kearney
+% Copyright 2015-2016 Kelly Kearney
 
 %--------------------
 % Parse inputs
@@ -50,108 +50,71 @@ Opt = parsepv(Opt, varargin);
 % Data setup
 %--------------------
 
-% Ecopath calculations
-
-Ep = ecopathlite(Ewein, 'silent', true);
-
-% Links show flow.  For now, not showing flow back to detritus (makes
-% things far too messy in most cases).
-
-adj = Ep.flow(1:end-1, 1:end-2);  % For now, ignore outside fluxes and resp
-if ~Opt.showdet
-    adj(:,Ep.Idx.det) = 0;
-end
-
-% % Eliminate cannibalism (until I can get that working in the sankey script)
-% 
-% if any(diag(adj))
-%     warning('Self-cycles (cannibalism) cannot be displayed in current version');
-%     adj = adj .* ~eye(size(adj));
-% end
+EM = EM.sortbytrophic;
+G = EM.graph('oos', false, 'det', Opt.showdet);
 
 % Scale data if necessary
 
-val = adj;
-val(val~=0) = Opt.linkscale(val(val~=0));
-
-% Node names
-
-if ~isfield(Ewein, 'fleet')
-    Ewein.fleet = cellstr(num2str((1:Ewein.ngear)', 'Fleet%d'));
-end
-
-grp = [Ewein.fleet; Ewein.name];
-nnode = length(grp);
+G.Edges.Weight = Opt.linkscale(G.Edges.Weight);
 
 % Rearrange so gears are first (not really necessary, but this
-% makes it easier to diagnose some errors since webs are usually ordered
-% top to bottom of the food chain)
+% makes it easier to diagnose some errors groups are sorted by trophic
+% level, top to bottom) 
 
-order = [Ewein.ngroup+1:Ewein.ngroup+Ewein.ngear 1:Ewein.ngroup];
-adj = adj(order, order);
-val = val(order, order);
+order = [EM.ngroup+(1:EM.ngear) 1:EM.ngroup];
+G = reordernodes(G, order);
 
+nnode = EM.ngroup + EM.ngear;
 idx = 0:(nnode-1);
 
-% Set the x-position of nodes based on trophic level.  Put detritus 1 layer
-% below lowest and fleet 1 layer above
+% Set the x-position of nodes based on trophic level.  
 
-tl = round(Ep.trophic./Opt.round) * Opt.round;
-tl = [ones(Ewein.ngear,1)*max(tl); tl];
-
+tl = round(G.Nodes.TL./Opt.round) * Opt.round;
 dl = min(diff(unique(tl)));
 
 layer = tl./dl;
-layer(1:Ewein.ngear) = max(layer) + 1;
 
-tlfull = [nan(Ewein.ngear,1); Ep.trophic];
+tlfull = tl;
 tlround = layer .* dl; % To get fleet TL
 
 layer = layer - min(layer);
-
 
 %--------------------
 % Write to JSON file
 %--------------------
 
-% Nodes
+Ep = EM.ecopath;
 
-G.nodes = struct('node', num2cell(idx), ...
-                 'name', grp', ...
-                 'layer', num2cell(layer'), ...
-                 'B', num2cell([nan(Ewein.ngear,1); Ep.b])', ...
-                 'PB', num2cell([nan(Ewein.ngear,1); Ep.pb])', ...
-                 'QB', num2cell([nan(Ewein.ngear,1); Ep.qb])', ...
-                 'EE', num2cell([nan(Ewein.ngear,1); Ep.ee])', ...
-                 'TLf', num2cell(tlfull'), ...
-                 'TLr', num2cell(tlround'));
+% Reformat for JSON structure
 
+Json.nodes = struct('nodes', num2cell(idx), ...
+                    'name', G.Nodes.Name', ...
+                    'layer', num2cell(layer'), ...
+                    'B',   num2cell([nan(EM.ngear,1); Ep.b])', ...
+                    'PB',  num2cell([nan(EM.ngear,1); Ep.pb])', ...
+                    'QB',  num2cell([nan(EM.ngear,1); Ep.qb])', ...
+                    'EE',  num2cell([nan(EM.ngear,1); Ep.ee])', ...
+                    'TLf', num2cell(tlfull'), ...
+                    'TLr', num2cell(tlround'));
+                
 
-             
-% Links
-             
-[isrc,isnk] = find(val);
-vidx = sub2ind(size(val), isrc, isnk);
-val = val(vidx);
-q = adj(vidx);
-
-% val = adj(find(adj));
-
+val = G.Edges.Weight;
 if min(val) < 0
     val = (min(val)) + 0.01*diff(minmax(val));
 end
+    
+Json.links = struct('source', num2cell(findnode(G, G.Edges.EndNodes(:,1))'-1), ...
+                    'target', num2cell(findnode(G, G.Edges.EndNodes(:,2))'-1), ...
+                    'value',  num2cell(val'), ...
+                    'Q',      num2cell(G.Edges.Weight'));
 
-G.links = struct('source', num2cell(idx(isrc)), ...
-                 'target', num2cell(idx(isnk)), ...
-                 'value',  num2cell(val'), ...
-                 'Q',      num2cell(q'));
-             
+
 % Save to file
              
 [pth,fl,ex] = fileparts(file);
 if isempty(ex)
     file = fullfile(pth, [fl '.json']);
-elseif ~strcmp(ex, 'json')
+elseif ~strcmp(ex, '.json')
     warning('File should be a .json file');
 end
 
@@ -159,6 +122,6 @@ Jopt = struct('Compact', 0, ...
              'FileName', file, ...
              'NoRowBracket', 1);
          
-savejson('', G, Jopt);
+savejson('', Json, Jopt);
 
 
